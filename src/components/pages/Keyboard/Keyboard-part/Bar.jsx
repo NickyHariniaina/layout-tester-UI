215b0623f06bcf951888e8d9bf79890ea03c45e2
NickyHariniaina <NickyHarini1na@gmail.com>
2025-03-04 18:14:44 +0300

    [FEAT] Loading and error message

--- src/components/pages/Keyboard/Keyboard-part/Bar.jsx
+++ src/components/pages/Keyboard/Keyboard-part/Bar.jsx
@@ -1,8 +1,8 @@
 export const Bar = ({ inputId, minute, second }) => {
 
     return <div className="row">
-        <button htmlFor={inputId} className="btn m-2 btn-light col">Start</button>
-        <button className="btn btn-success m-2 col">Submit</button>
+        <button htmlFor={inputId} className="btn m-2 btn-outline-light col">Start</button>
+        <button className="btn btn-outline-success m-2 col">Submit</button>
         <button className="btn btn-outline-warning m-2 col">Pause</button>
         <button className="btn btn-outline-danger m-2 col">Reset</button>
         <div className="timer text-light fs-2 text-center">{minute}:{second}</div>
--- src/components/pages/Keyboard/Keyboard-part/TesterInput.jsx
+++ src/components/pages/Keyboard/Keyboard-part/TesterInput.jsx
@@ -1,44 +1,74 @@
-import { useState, useId } from "react"
+import { useState, useId, useEffect } from "react"
+import { Loading } from "./Loading.jsx"
 import { Bar } from "./Bar"
-
-export const TesterInput = ({ word, onKeyDown, wordInde })  => {
+import { ErrorNotification } from "./ErrorNotification.jsx"
+export const TesterInput = ({ onKeyDown })  => {
+    const [loading, setLoading] = useState(true)
     const userInput = document.querySelector(".userInput")
     const inputId = useId()
     const [typed, setTyped] = useState("Type...")
     const [minute, setMinute] = useState(0)
     const [second, setSecond] = useState(30)
+    const [word, setWord] = useState("")
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+      const fetchAPI = async (url) => {
+          try {
+            const data = await fetch(url)
+            if (data.ok) {
+              let Jdata = await data.json()
+              setWord(Jdata.join(" "))
+              setLoading(!loading)
+            }
+          } catch (e) {
+            setError(!error)
+          }
+      }
+      fetchAPI("https://random-word-api.herokuapp.com/word?number=42")
+    }, [])
 
     const touch = /[a-zA-Z0-9]/
 
     const handleKeyDown = (event) => {
-		if (event.key === " ") {
-			setTyped("")
-			userInput.value = ""
-		}
-		else if (touch.test(event.key) && event.key.length === 1) { 
-            setTyped(text => text + event.key)
-            if (typed === "Type...") setTyped(text => touch.test(event.key) ? event.key : " ")
-        } else if (event.key === "Backspace") {
-            setTyped(text => {
-                text = text.split("")
-                text.pop()
-                return text.join("")
-            })
-        }
-        
-	}
+      if (event.key === " ") {
+        setTyped("")
+        userInput.value = ""
+      }
+      else if (touch.test(event.key) && event.key.length === 1) { 
+              setTyped(text => text + event.key)
+              if (typed === "Type...") setTyped(text => touch.test(event.key) ? event.key : " ")
+          } else if (event.key === "Backspace") {
+              setTyped(text => {
+                  text = text.split("")
+                  text.pop()
+                  return text.join("")
+              })
+          }          
+    }
+
+    // FIXME: Separate all of this shit in different folder...
 
     return <div className="container-fluid bg-dark border rounded my-1" onKeyDown={handleKeyDown}>
         <div className="keyboard__tester-input__word fs-1 px-4 py-1 text-light text-nowrap px-2">
-            {word}
+              {
+                loading? <Loading /> : word
+              }
         </div>
         <div className="keyboard__tester-input__answer fs-1 px-4 py-1 text-light text-nowrap"> 
-            Nothing in the world
+            {
+              "Here" // FIXME: We can't see the users last input...
+            }
         </div>
         <div className="keyboard__tester-input__preview fs-4 my-2 py-1 mx-auto w-25 text-center bg-light rounded">
             {typed}
         </div>
         <input type="text" className="form-control w-25 mx-auto my-2 userInput d-none" id={inputId} />
         <Bar inputId={inputId} minute={minute} second={second}/>
+        <div className=""> 
+          {
+            error? <ErrorNotification />: null //TODO: Improve succes notification later 
+          }
+        </div>
     </div>
 }
