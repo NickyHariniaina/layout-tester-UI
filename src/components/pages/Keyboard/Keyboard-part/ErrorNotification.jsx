644ae2e1f61fc9d0b5b2ec45d2d1ed33afb82e69
NickyHariniaina <NickyHarini1na@gmail.com>
2025-03-04 18:17:18 +0300

    [ADD] loading and Error notification

---
+++ src/components/pages/Keyboard/Keyboard-part/ErrorNotification.jsx
@@ -0,0 +1,12 @@
+export const ErrorNotification = () => {
+
+  // TODO: Add a better notification...
+
+  return <div>
+    <div class="alert alert-danger d-flex align-items-center" role="alert">
+      <div>
+        Please verify your internet connexion...
+      </div>
+    </div>
+  </div> 
+}
---
+++ src/components/pages/Keyboard/Keyboard-part/Loading.jsx
@@ -0,0 +1,8 @@
+export const Loading = () => {
+  return <div className=""> 
+    <div class="d-flex align-items-center">
+      <p role="status">Please wait...</p>
+      <div class="spinner-border ms-auto" aria-hidden="true"></div>
+    </div>
+  </div>
+}
