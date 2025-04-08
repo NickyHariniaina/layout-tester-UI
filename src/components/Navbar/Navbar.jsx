a2ede4256d091f6548d551b85a36976492622ad7
NickyHariniaina <NickyHarini1na@gmail.com>
2025-04-08 16:29:38 +0300

    improve: change all background and add hover.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,9 +4,9 @@ export const Navbar = ({ link1, link2, link3, link4,
     option1, option2, option3, option4 }) => {
     
     return <nav className="nav nav-pills flex-column flex-sm-row">
-        <Link className="flex-sm-fill text-sm-center nav-link text-light " to={link1}>{option1}</Link>
-        <Link className="flex-sm-fill text-sm-center nav-link text-light" to={link2}>{option2}</Link>
-        <Link className="flex-sm-fill text-sm-center nav-link text-light" to={link3}>{option3}</Link>
-        <Link className="flex-sm-fill text-sm-center nav-link text-light" to={link4}>{option4}</Link>
+        <Link className="nav__item flex-sm-fill text-sm-center nav-link text-dark " to={link1}>{option1}</Link>
+        <Link className="nav__item flex-sm-fill text-sm-center nav-link text-dark" to={link2}>{option2}</Link>
+        <Link className="nav__item flex-sm-fill text-sm-center nav-link text-dark" to={link3}>{option3}</Link>
+        <Link className="nav__item flex-sm-fill text-sm-center nav-link text-dark" to={link4}>{option4}</Link>
   </nav> 
 }
